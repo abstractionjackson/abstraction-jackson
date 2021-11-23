c20885607ca2c228a362449649e19d7bbc3b6ba3
abstractionjackson <47265694+abstractionjackson@users.noreply.github.com>
2021-11-22 21:38:58 -0800

    update env vars

--- config/s3.config.js
+++ config/s3.config.js
@@ -1,8 +1,8 @@
 import { S3Client } from "@aws-sdk/client-s3"
 const client = new S3Client({
     credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || ''
+      accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID || '',
+      secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY || ''
   },
   region: 'us-west-1'
 })
